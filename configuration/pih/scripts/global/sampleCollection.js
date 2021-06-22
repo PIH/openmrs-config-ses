5890ab4732dd908c6b913d8a032c32f4d94ebe65
Miguel <mhuamani_ses@pih.org>
2021-06-22 18:22:26 -0500

    SES configuration files for national documents

--- configuration/pih/scripts/global/sampleCollection.js
+++ configuration/pih/scripts/global/sampleCollection.js
@@ -10,43 +10,38 @@ $(() => {
 	  const encounter = await $.get(`/openmrs/ws/rest/v1/encounter/${encounterId}`);
 	  //examenes=orders.display;
 	  let fechaVisitaValue=new Date(Date.parse(encounter.encounterDatetime)).toLocaleString();
-	  //let examenes1=[orders];
-	  console.log(encounter);
+	  let examenes=[];
+	  //console.log(encounter);
 	  $("#fecha").text(new Date(Date.parse(encounter.encounterDatetime)).toLocaleString());
 	  $("#local").text(encounter.location.display);
 	  $("#proyecto").text(encounter.location.display);
 	  $.each(encounter.orders, (i, orders) => {
-		console.log(orders);
-		examenes=orders.display;
-		$("#examenes").append($(`<td>${examenes}</td>`));
-	  });
-	  // Subsección: Concepto - Tipo de documento
-	  const tipoDocumento = await $.get(`/openmrs/ws/rest/v1/obs?patient=${patientId}&concept=b105fdb9-15de-48cd-a741-6c8b6a6bece8`);
-	  //let tipoDocumentoValue = tipoDocumento.results;
-	  $.each(tipoDocumento.results, (i, results) => {
-		tipoDocumentoValue = results.display.split(': ')[1];
-		$("#documento").text(tipoDocumentoValue);
-	  });
-	  // Subsección: Concepto - Número de documento
-	  const numeroDocumento = await $.get(`/openmrs/ws/rest/v1/obs?patient=${patientId}&concept=87670a92-bc08-4434-9396-43a440d6f29e`);
-	  //let numeroDocumentoRaw = null;
-	  $.each(numeroDocumento.results, (i, results) => {
-		numeroDocumentoValue = results.display.split(': ')[1];
-		$("#numerodocumento").text(numeroDocumentoValue);
+		//console.log(orders);
+		examenes.push(orders.display);
+		$("#examenes").append($(`<td>${orders.display}</td>`));
 	  });
 	  const paciente= await $.get(`/openmrs/ws/rest/v1/patient/${patientId}`);
-	  let nombresValue=paciente.person.display;
+	  let nombresValue=paciente.person.display.split(" ")[2];
+	  let apePaternoValue=paciente.person.display.split(" ")[1].slice(0,-1);
+	  let apeMaternoValue=paciente.person.display.split(" ")[0];
 	  let fecnac=new Date(Date.parse(paciente.person.birthdate)).toLocaleDateString('en-ZA');
 	  let fechaNacimientoValue=fecnac.substring(0,10);
 	  let sexoValue=paciente.person.gender;
+	  $.each(paciente.identifiers, (i=1, data) => {
+		  console.log(data);
+		  tipoDocumentoValue = data.display.split('= ')[0];
+		  $("#documento").text(tipoDocumentoValue);
+		  numeroDocumentoValue = data.display.split('= ')[1];
+		  $("#numerodocumento").text(numeroDocumentoValue);
+	  });
 	  // Sección: Llamadas a API - SEIS
 	  const request = {
 		Paciente: {
 			TipoDocumento: tipoDocumentoValue,
 			NumeroDocumento: numeroDocumentoValue,
 			Nombres: nombresValue,
-			ApellidoPaterno: null,
-			ApellidoMaterno: null,
+			ApellidoPaterno: apePaternoValue,
+			ApellidoMaterno: apeMaternoValue,
 			FechaNacimiento: fechaNacimientoValue,
 			Sexo: sexoValue
 		},
@@ -54,7 +49,7 @@ $(() => {
 			FechaVisita: fechaVisitaValue
 		},
 		SolicitudAnalisis: {
-			Examenes: null
+			Examenes: examenes
 		}
 	  };
 	  console.log("Objeto de solicitud a API SEIS");
